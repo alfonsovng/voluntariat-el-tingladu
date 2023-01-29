22b6cbe7d833dbc3d6e9a58a32368da14615fd1e
Alfonso da Silva <asilva5@xtec.cat>
2023-01-29 08:07:48 +0100

    Create out of order static page

--- service-postgres/sql/3_tasks.sql
+++
@@ -1,2 +0,0 @@
-\copy tasks(id, name, description, password) from '/docker-entrypoint-initdb.d/csv/tasks.csv' delimiter ',' csv header;
-SELECT pg_catalog.setval('public.tasks_id_seq', (SELECT MAX(id) FROM tasks));
--- service-postgres/sql/6_rewards.sql
+++
@@ -1,2 +0,0 @@
-\copy rewards(id, name, description) from '/docker-entrypoint-initdb.d/csv/rewards.csv' delimiter ',' csv header;
-SELECT pg_catalog.setval('public.rewards_id_seq', (SELECT MAX(id) FROM rewards));
