eb36944329cb258273421832182903eec54103f6
Alfonso da Silva <asilva5@xtec.cat>
2023-02-07 22:54:01 +0100

    Afegit codi als meals i rewards per a comprovar integratitat

--- service-flask/voluntariat_app/plugin_usher.py
+++ service-flask/voluntariat_app/plugin_usher.py
@@ -1,18 +1,26 @@
 from .helper import logger
 
 class UsherImpl:
-    def get_description(self):
-        raise NotImplementedError
-
     def calculate_rewards(self, user_id, current_shifts):
         raise NotImplementedError
 
     def calculate_meals(self, user_id, current_shifts):
         raise NotImplementedError
 
+    def _get_meal_id(self, db, code):
+        return db.session.execute(f"select id from meals where code = '{code}'").one()
+
+    def _get_reward_id(self, db, code):
+        return db.session.execute(f"select id from rewards where code = '{code}'").one()
+
 class Usher15Anniversary(UsherImpl):
-    def get_description(self):
-        return "15è aniversari del Tingladu el 13 de maig de 2023"
+
+    def __init__(self, app, db):
+        with app.app_context():
+            self.sopar_15_aniversari_id = self._get_meal_id(db, "sopar_15_aniversari")
+            self.entrada_15_aniversari_id = self._get_reward_id(db, "entrada_15_aniversari")
+
+        logger.info(f"15è aniversari del Tingladu el 13 de maig de 2023: sopar_15_aniversari_id={self.sopar_15_aniversari_id}, entrada_15_aniversari_id={self.entrada_15_aniversari_id}")
 
     def calculate_rewards(self, user_id, current_shifts):
         from .models import UserReward
@@ -24,8 +32,8 @@ class Usher15Anniversary(UsherImpl):
         # No es pot renunciar.
         reward = UserReward(
             user_id = user_id,
-            options = [1],
-            selected = 1
+            options = [self.entrada_15_aniversari_id],
+            selected = self.entrada_15_aniversari_id
         )
 
         return [reward]
@@ -40,7 +48,7 @@ class Usher15Anniversary(UsherImpl):
         # Es pot renunciar. No surt seleccionat per defecte
         meal = UserMeal(
             user_id = user_id,
-            options = [0,1],
+            options = [0,self.sopar_15_aniversari_id],
             selected = 0
         )
 
@@ -52,11 +60,10 @@ class UsherManager:
         if app is not None:
             self.init_app(app)
 
-    def init_app(self, app):
+    def init_app(self, app, db):
         dynamic_class_name = app.config.get('USHER_CLASS')
-        self.usher_instance =  globals()[dynamic_class_name]()
-
-        logger.info(f"USHER_CLASS = {dynamic_class_name}: {self.usher_instance.get_description()}")
+        logger.info(f"USHER_CLASS = {dynamic_class_name}")
+        self.usher_instance =  globals()[dynamic_class_name](app, db)
 
     def update_rewards(self, user_id, current_shifts):
         from .models import UserReward
--- service-postgres/sql/6_rewards.sql
+++ service-postgres/sql/6_rewards.sql
@@ -1,2 +1,2 @@
-\copy rewards(id, name, description) from '/docker-entrypoint-initdb.d/csv/rewards.csv' delimiter ',' csv header;
+\copy rewards(id, code, name, description) from '/docker-entrypoint-initdb.d/csv/rewards.csv' delimiter ',' csv header;
 SELECT pg_catalog.setval('public.rewards_id_seq', (SELECT MAX(id) FROM rewards));
